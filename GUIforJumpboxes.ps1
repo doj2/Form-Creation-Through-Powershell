89f06f98992feff58b8cfc45f33a88d6f4b21290
Johnny Do <55112984+doj2@users.noreply.github.com>
2021-11-12 23:52:50 -0500

    Add files via upload

---
+++ GUIforJumpboxes.ps1
@@ -0,0 +1,81 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Select a Device'
+$form.Size = New-Object System.Drawing.Size(300,260)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.BackColor = "green"
+$okButton.Location = New-Object System.Drawing.Point(75,190)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.BackColor = "red"
+$cancelButton.Location = New-Object System.Drawing.Point(150,190)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please select a device:'
+$form.Controls.Add($label)
+
+$listBox = New-Object System.Windows.Forms.ListBox
+$listBox.Location = New-Object System.Drawing.Point(10,40)
+$listBox.Size = New-Object System.Drawing.Size(260,20)
+$listBox.Height = 80
+
+$listBox.SelectionMode = 'MultiExtended'
+
+[void] $listBox.Items.Add('atl-dc-001')
+[void] $listBox.Items.Add('atl-dc-002')
+[void] $listBox.Items.Add('atl-dc-003')
+[void] $listBox.Items.Add('atl-dc-004')
+[void] $listBox.Items.Add('atl-dc-005')
+[void] $listBox.Items.Add('atl-dc-006')
+[void] $listBox.Items.Add('atl-dc-007')
+
+$form.Controls.Add($listBox)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(130,120)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Or'
+$form.Controls.Add($label)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,140)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please input the desired device:'
+$form.Controls.Add($label)
+
+#creating a input text box
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,160)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+$form.Topmost = $true
+
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $y = $listBox.SelectedItems
+    $y
+}
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $textBox.Text
+    $x
+}
